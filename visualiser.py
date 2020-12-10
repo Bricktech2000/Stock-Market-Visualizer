b5fca584cb284b3ade78b4af3e383d7025af8f95
Bricktech2000 <bricktech2000@gmail.com>
2020-12-09 19:39:53 -0500

    added yfinance stock data

--- visualiser.py
+++ visualiser.py
@@ -3,6 +3,8 @@
 #https://stackoverflow.com/questions/64729944/runtimeerror-the-current-numpy-installation-fails-to-pass-a-sanity-check-due-to
 #pip install numpy==1.19.3
 import matplotlib.pyplot as plt
+#https://pypi.org/project/yfinance/
+import yfinance as yf
 
 
 
@@ -42,15 +44,36 @@ data = [
     [3, 10.0],
 ]
 
+def getFromTicker(ticker):
+    ticker = yf.Ticker(ticker)
+    data = ticker.history(period='1d', interval='1m')
+    #Open, High, Low, Close, Volume, Dividends, Stock Splits
+    return data
+
+
+def compile(data):
+    data2 = []
+    for i in range(0, len(data)):
+        data2.append([i, data['Open'][i]])
+        data2.append([i, data['High'][i]])
+        data2.append([i, data['Low'][i]])
+        data2.append([i, data['Close'][i]])
+    out = []
+    while len(data2) > 0:
+        item = data2.pop(0)
+        if not (item in data2):
+            out.append(item)
+    return out
+
 def visualize(points):
     #https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/marker_reference.html
     #https://stackoverflow.com/questions/21519203/plotting-a-list-of-x-y-coordinates-in-python-matplotlib
     plt.scatter([point[0] for point in points], [point[1] for point in points], marker='s')
     plt.show()
 
-def compile(data):
-    return data
 
+data = getFromTicker('AAPL')
+print(data)
 points = compile(data)
 visualize(points)
 
